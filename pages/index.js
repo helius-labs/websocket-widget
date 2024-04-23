c5b3737fc4ecc4f482184eb56230442d0a1e085c
Amy Qin <amyqin501@gmail.com>
2024-04-23 14:35:37 -0400

    Replace Link and open in new tab

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,6 @@ import { PublicKey } from "@solana/web3.js";
 import { ClockIcon, ArrowRightCircleIcon, CubeTransparentIcon, ChatBubbleOvalLeftEllipsisIcon, XCircleIcon, ArrowTopRightOnSquareIcon } from "@heroicons/react/24/outline";
 import { CursorArrowRaysIcon, SunIcon, MoonIcon } from "@heroicons/react/20/solid";
 import { Switch, Dialog, Transition, Popover } from "@headlessui/react";
-import Link from "next/link";
 
 const classNames = (...classes) => {
     return classes.filter(Boolean).join(' ');
@@ -251,9 +250,11 @@ export default function Home() {
                 </Transition.Root>
 
                 <nav className="flex items-center justify-between p-10">
-                    <Link
+                    <a
                         href="https://www.helius.dev/"
                         className=""
+                        href="https://github.com/helius-labs/websocket-widget"
+                        target="_blank"
                     >
                         <svg width="46" height="46" viewBox="0 0 494 490" fill="none" xmlns="http://www.w3.org/2000/svg">
                             <path d="M243.508 0.638046C242.84 0.944397 241.092 3.29308 239.55 5.79493C235.028 13.5558 183.787 101.733 183.787 101.835C183.787 101.938 186.151 101.07 189.029 99.9463C226.085 85.7011 268.537 86.0585 306.518 101.019C308.008 101.631 309.139 101.938 309.036 101.733C308.882 101.478 295.931 79.1656 280.204 52.0537C264.477 24.9418 250.909 2.16979 250.035 1.35286C248.39 -0.127823 245.718 -0.434174 243.508 0.638046Z" fill="white" />
@@ -271,7 +272,7 @@ export default function Home() {
                             <path d="M429.597 361.045C420.023 362.885 410.812 368.201 404.861 375.305C391.095 391.661 391.043 414.508 404.705 430.864C406.361 432.858 408.431 434.953 409.259 435.567L410.863 436.64L428.924 414.304C462.199 373.107 464.735 369.938 464.735 369.632C464.735 368.967 458.059 364.981 454.54 363.549C452.625 362.732 449.21 361.709 447.036 361.3C442.741 360.432 433.478 360.278 429.597 361.045Z" fill="white" />
                             <path d="M237.515 447.894C227.027 450.181 217.259 456.788 211.347 465.581C206.977 472.087 204.715 478.237 204.047 485.556L203.687 489.318H246.974H290.313L289.953 485.861C287.948 467.208 275.096 452.417 257.153 448.148C252.167 446.979 242.399 446.826 237.515 447.894Z" fill="white" />
                         </svg>
-                    </Link>
+                    </a>
                     <div className="flex flex-row space-x-4">
                         <button
                             onClick={toggleTheme}
@@ -290,8 +291,10 @@ export default function Home() {
                                 </>
                             )}
                         </button>
-                        <Link
+                        <a 
                         href="https://github.com/helius-labs/websocket-widget"
+                        target="_blank"
+                        rel="noopener noreferrer"
                         className="flex justify-center items-center ring-1 ring-white/50 hover:ring-orange-300/50 rounded-full h-8 px-4 transition-color duration-200 ease-in-out hover:bg-white/10"
                     >
                         View Source
@@ -307,7 +310,7 @@ export default function Home() {
                         >
                             <path d="M15,3C8.373,3,3,8.373,3,15c0,5.623,3.872,10.328,9.092,11.63C12.036,26.468,12,26.28,12,26.047v-2.051 c-0.487,0-1.303,0-1.508,0c-0.821,0-1.551-0.353-1.905-1.009c-0.393-0.729-0.461-1.844-1.435-2.526 c-0.289-0.227-0.069-0.486,0.264-0.451c0.615,0.174,1.125,0.596,1.605,1.222c0.478,0.627,0.703,0.769,1.596,0.769 c0.433,0,1.081-0.025,1.691-0.121c0.328-0.833,0.895-1.6,1.588-1.962c-3.996-0.411-5.903-2.399-5.903-5.098 c0-1.162,0.495-2.286,1.336-3.233C9.053,10.647,8.706,8.73,9.435,8c1.798,0,2.885,1.166,3.146,1.481C13.477,9.174,14.461,9,15.495,9 c1.036,0,2.024,0.174,2.922,0.483C18.675,9.17,19.763,8,21.565,8c0.732,0.731,0.381,2.656,0.102,3.594 c0.836,0.945,1.328,2.066,1.328,3.226c0,2.697-1.904,4.684-5.894,5.097C18.199,20.49,19,22.1,19,23.313v2.734 c0,0.104-0.023,0.179-0.035,0.268C23.641,24.676,27,20.236,27,15C27,8.373,21.627,3,15,3z"></path>
                         </svg>
-                    </Link>
+                    </a>
                     </div>
                 </nav>
 
